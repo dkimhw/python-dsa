9505d1e6d290c7749db78c5e3280c9ddd163184b
David Kim <dkhw90@gmail.com>
2024-10-10 01:14:32 +0200

    Solve anagrams & apply big O notation

--- monkeys/monkeys.py
+++ monkeys/monkeys.py
@@ -1,5 +1,7 @@
 import random
 
+TARGET = "methinks it is like a weasel"
+
 def generate_random_string(length):
   chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
   random_str = ''
@@ -9,5 +11,33 @@ def generate_random_string(length):
 
   return random_str
 
+def score_string(str):
+  correct_chars = 0
+  for idx, _ in enumerate(str):
+    if str[idx] == TARGET[idx]:
+      correct_chars += 1
 
-print(generate_random_string(28))
+  return correct_chars / len(str)
+
+def start_monkeys():
+  curr_score = 0
+  best_score = 0
+  best_string = ""
+  attempt_number = 0
+
+  while curr_score < 1.0:
+    random_str = generate_random_string(28)
+    curr_score = score_string(random_str)
+
+    if curr_score > best_score:
+      best_score = curr_score
+      best_string = random_str
+      print(attempt_number, best_score, best_string)
+
+    attempt_number += 1
+
+
+
+
+
+start_monkeys()
